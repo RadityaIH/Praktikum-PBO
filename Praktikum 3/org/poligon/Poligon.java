44783a6f0128378c537c67f496a652e45c7a0200
RadityaIH <100180125+RadityaIH@users.noreply.github.com>
2023-03-09 20:30:37 +0700

    Add files via upload

---
+++ Praktikum 3/org/poligon/Poligon.java
@@ -0,0 +1,14 @@
+/**
+* File: Poligon.java 08/03/2023
+* Penulis: Raditya Ilham Hastoro
+* Deskripsi: representasi dasar dari objek poligon (segi banyak)
+*/
+package org.poligon;
+
+public class Poligon{
+	protected int jumlahSisi;
+	
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+}
