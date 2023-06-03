87952701e107f54e873374ee31dc7a6cdb462887
RadityaIH <100180125+RadityaIH@users.noreply.github.com>
2023-06-03 12:06:40 +0700

    Add files via upload

---
+++ Praktikum 10/LambdaHashmap.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * File: LambdaHashmap.java
+ * Deskripsi: Implementasi Lambda pada Hashmap, digunakan sebagai parameter pada method
+ */
+
+public class LambdaHashmap 
+{
+    public static void main(String[] args) 
+    {
+        Map<Integer, String> mahasiswaMap = new HashMap<>();
+        mahasiswaMap.put(12345, "Adi");
+        mahasiswaMap.put(67890, "Bambang");
+        mahasiswaMap.put(54321, "Cici");
+        mahasiswaMap.put(98765, "Didi");
+
+        mahasiswaMap.forEach((nim, nama) -> System.out.println("NIM: " + nim + ", Nama: " + nama));
+    }
+}
